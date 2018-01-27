8ec0407d35d907ee1a8881a5821ca9abd7c9f461
Jim Speaker <jim@IONTECH.org>
2018-01-27 12:41:00 -0800

    Ready for refactor to chain of strategies.

--- FizzBuzz_20180127_1/FizzBuzzTests.cs
+++ FizzBuzz_20180127_1/FizzBuzzTests.cs
@@ -58,25 +58,4 @@ namespace FizzBuzz_20180127_1
             buzz.Should().Be("fizzbuzz");
         }
     }
-
-    public class FizzBuzz
-    {
-        private readonly int _value;
-
-        public FizzBuzz(int value)
-        {
-            _value = value;
-        }
-
-        public override string ToString()
-        {
-            if (_value % 15 == 0) return "fizzbuzz";
-
-            if (_value % 5 == 0) return "buzz";
-
-            if (_value % 3 == 0) return "fizz";
-
-            return _value.ToString();
-        }
-    }
 }
---
+++ FizzBuzz_20180127_1/StrategyTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz_20180127_1
+{
+    [TestFixture]
+    public class StrategyTests
+    {
+        
+    }
+}
