aa4381351db1934f68d28d782b78bb5752a699ee
Jim Speaker <jim@IONTECH.org>
2018-01-27 12:28:53 -0800

    Initial TDD'd FizzBuzz solution - ready for branching / experimentation

---
+++ FizzBuzz_20180127_1/FizzBuzzTests.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace FizzBuzz_20180127_1
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test, Category("Unit")]
+        public void GivenTwo_WhenAskingToString_ThenItShouldReturnTwoAsString()
+        {
+            // arrange
+            FizzBuzz fizzBuzz = new FizzBuzz(2);
+
+            // act
+            string one = fizzBuzz.ToString();
+
+            // assert
+            one.Should().Be("2");
+        }
+
+        [Test, Category("Unit")]
+        public void GivenSix_WhenAskingToString_ThenItShouldReturnFizz()
+        {
+            // arrange
+            FizzBuzz fizzBuzz = new FizzBuzz(6);
+
+            // act
+            string fizz = fizzBuzz.ToString();
+
+            // assert
+            fizz.Should().Be("fizz");
+        }
+
+        [Test, Category("Unit")]
+        public void GivenTen_WhenAskingToString_ThenItShouldReturnBuzz()
+        {
+            // arrange
+            FizzBuzz fizzBuzz = new FizzBuzz(10);
+
+            // act
+            string buzz = fizzBuzz.ToString();
+
+            // assert
+            buzz.Should().Be("buzz");
+        }
+
+        [Test, Category("Unit")]
+        public void GivenThirty_WhenAskingToString_ThenItShouldReturnFizzBuzz()
+        {
+            // arrange
+            FizzBuzz fizzBuzz = new FizzBuzz(30);
+
+            // act
+            string buzz = fizzBuzz.ToString();
+
+            // assert
+            buzz.Should().Be("fizzbuzz");
+        }
+    }
+
+    public class FizzBuzz
+    {
+        private readonly int _value;
+
+        public FizzBuzz(int value)
+        {
+            _value = value;
+        }
+
+        public override string ToString()
+        {
+            if (_value % 15 == 0) return "fizzbuzz";
+
+            if (_value % 5 == 0) return "buzz";
+
+            if (_value % 3 == 0) return "fizz";
+
+            return _value.ToString();
+        }
+    }
+}
---
+++ FizzBuzz_20180127_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzz_20180127_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FizzBuzz_20180127_1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89d96e55-174e-4300-bef0-165b52faabdf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
